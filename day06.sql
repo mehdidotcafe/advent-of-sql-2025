bb072d91ed84bbcdbe3bb6af6d36ec83ea8368a8
mehdidotcafe <contact@mehdi.cafe>
2025-12-13 21:47:58 +0100

    day06

---
+++ day06.sql
@@ -0,0 +1,10 @@
+SELECT
+  t.name
+FROM
+  toys as t
+  JOIN production_logs pl ON pl.toy_id = t.id
+  JOIN production_summary ps ON ps.toy_id = t.id
+GROUP BY
+  t.id
+HAVING
+  SUM(pl.quantity) != ps.expected_total
